5cbb2d73e00cd783b00e9c1964f7799cd0d95521
masquevil <me@soulran.com>
2014-08-20 17:55:32 +0800

    2

---
+++ foreign/renrenbeautifier.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name       RenRen Beautifier | For V7
+// @namespace  http://www.soulran.com/
+// @version    0.11
+// @description  Remove ad and something I dislike.
+// @match      http://*.renren.com/*
+// @copyright  2013+, Masquevil
+// ==/UserScript==
+
+var removeAdApp = function(){
+	var rrSlidebar = document.getElementById("nxSlidebar");
+    var rrAdAppList = rrSlidebar.getElementsByTagName("div");
+    for (var num in rrAdAppList){
+        if((/recent/).test(rrAdAppList[num].className)){
+            rrAdAppList[num].parentNode.removeChild(rrAdAppList[num]);
+        }
+    }
+}
+removeAdApp();
+
+//Just 1
