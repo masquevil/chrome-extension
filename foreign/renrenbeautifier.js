1f32dfed7fa1a541729e5c69bb164f95f895e0f2
masquevil <me@soulran.com>
2014-08-21 23:11:31 +0800

    bug removed

--- foreign/renrenbeautifier.js
+++ foreign/renrenbeautifier.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       RenRen Beautifier | For V7
 // @namespace  http://www.soulran.com/
-// @version    0.11
+// @version    0.12
 // @description  Remove ad and something I dislike.
 // @match      http://*.renren.com/*
 // @run-at      document-end
@@ -10,12 +10,27 @@
 
 var removeAdApp = function(){
 	var rrSlidebar = document.getElementById("nxSlidebar");
-    var rrAdAppList = rrSlidebar.getElementsByTagName("div");
-    for (var num in rrAdAppList){
+    if(rrSlidebar)var rrAdAppList = rrSlidebar.getElementsByTagName("div");
+    if(rrAdAppList)for (var num in rrAdAppList){
         if((/recent/).test(rrAdAppList[num].className)){
             rrAdAppList[num].parentNode.removeChild(rrAdAppList[num]);
         }
     }
 }
+function itttfun(times){
+	try{
+		var itttfunOKK = window.unsafeWindow.itttfunOK || false;
+		if(itttfunOKK)return;
+		window.unsafeWindow.itttfunOK = true;
+		window.unsafeWindow.webpager.setFold(true);
+	} catch(e) {
+		window.unsafeWindow.itttfunOK = false;
+		if(times++ < 10)setTimeout(function(){itttfun(times);},600);
+	} finally{
+        if(window.unsafeWindow.itttfunOK){
+            window.unsafeWindow.document.getElementById("buddy").style.display="none";
+        }
+	}
+}
 removeAdApp();
-webpager.setFold(true);
+itttfun(0);
