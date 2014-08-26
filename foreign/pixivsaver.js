39771295b2ee2755d488b9ef2b0cb3a4f8ad5973
masquevil <me@soulran.com>
2014-08-26 22:00:55 +0800

    More type of image in pixiv saver.

--- foreign/pixivsaver.js
+++ foreign/pixivsaver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Pixiv Saver
 // @namespace  http://www.soulran.com/
-// @version    0.21
+// @version    0.22
 // @description  Download the image with "mode=medium" in pixiv in the complete size.
 // @match      http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
 // @run-at      document-end
@@ -11,7 +11,7 @@
 (function(){
 	var itttval = setInterval(chClick,500);
 	function chClick(){
-		var matt = document.body.innerHTML.match(/(http:\/\/i\d+\.pixiv\.net\/img\d+\/img\/\w+\/\d+)_m(\.jpg)/i);
+		var matt = document.body.innerHTML.match(/(http:\/\/i\d+\.pixiv\.net\/img\d+\/img\/.+?\/\d+)_m(\.)(jpg|png|jpeg|gif)/i);
 		var memm = document.body.innerHTML.match(/member_illust\.php\?mode=big&amp;illust_id=\d+\"/i);
 		if((matt == null || memm == null))return false;
 		document.body.innerHTML = document.body.innerHTML.replace(memm[0], matt[1] + matt[2] + "\" download");
