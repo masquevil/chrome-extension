762fcf52f9decaca5b88d32ff35cdfd5d516eb35
masquevil <me@soulran.com>
2014-08-26 14:10:42 +0800

    Add pixiv saver

---
+++ foreign/pixivsaver.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name       Pixiv Saver
+// @namespace  http://www.soulran.com/
+// @version    0.1
+// @description  Download the image with "mode=medium" in pixiv in the complete size.
+// @match      http://www.pixiv.net/member_illust.php?mode=medium&illust_id=*
+// @run-at      document-end
+// @copyright  2014+, Masquevil
+// ==/UserScript==
+
+(function(){
+	var itttval = setInterval(chClick,500);
+	function chClick(){
+		var matt = document.body.innerHTML.match(/(http:\/\/i\d+\.pixiv\.net\/img\d+\/img\/\w+\/\d+)_m(\.jpg)/i);
+		var memm = document.body.innerHTML.match(/member_illust\.php\?mode=big&amp;illust_id=\d+\"/i);
+		if((matt == null || memm == null))return false;
+		document.body.innerHTML = document.body.innerHTML.replace(memm[0], matt[1] + matt[2] + "\" download");
+		clearInterval(itttval);
+	}
+})();
